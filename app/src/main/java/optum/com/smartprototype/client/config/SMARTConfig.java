4ad3506d82527da87fb1ee1f134aa3163191c713
Edison <gedison@LAMU02RJ4B0G8WP.uhc.com>
2017-05-30 08:54:45 -0400

    Removed config information

--- app/src/main/java/optum/com/smartprototype/client/config/SMARTConfig.java
+++ app/src/main/java/optum/com/smartprototype/client/config/SMARTConfig.java
@@ -5,7 +5,7 @@ package optum.com.smartprototype.client.config;
  */
 
 public class SMARTConfig{
-/*
+///*
     public static final String BASE_URL =       "https://auth.hspconsortium.org";
     public static final String TOKEN_URL =      BASE_URL+"/token";
     public static final String AUTHORIZE_URL =  BASE_URL+"/authorize";
@@ -17,19 +17,6 @@ public class SMARTConfig{
     public static final String REDIRECT_URI =    "http://smartapp/callback";
     public static final String RESPONSE_TYPE =   "code";
     public static final String GRANT_TYPE =      "authorization_code";
-*/
-
-///*
-    public static final String BASE_URL =       "http://ec2-52-27-185-80.us-west-2.compute.amazonaws.com:8080/hspc-reference-authorization";
-    public static final String TOKEN_URL =      BASE_URL+"/token";
-    public static final String AUTHORIZE_URL =  BASE_URL+"/authorize";
-    public static final String DATA_URL =       "http://ec2-52-27-185-80.us-west-2.compute.amazonaws.com:8080/tortuga-1/baseDstu3";
-
-    public static final String STATE =           "3F9463CA";
-    public static final String SCOPE =           "openid user/*.* profile";
-    public static final String CLIENT_ID =       "a6d594f6-fd9e-40c1-8357-576379f71a6c";
-    public static final String REDIRECT_URI =    "http://smartapp/callback";
-    public static final String RESPONSE_TYPE =   "code";
-    public static final String GRANT_TYPE =      "authorization_code";
 //*/
+
 }
